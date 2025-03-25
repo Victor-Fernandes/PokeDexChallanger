9e1692197a38adc1fa38edcfdcfff8f38131d6db
Victor Fernandes <victor.fernandes.07@gmail.com>
2025-03-25 12:41:29 -0300

    Add HTTP exception filter and update main.ts
    Added new file backend/src/infrastructure/filters/http-exception-filter.ts to handle global HTTP exceptions. Modified backend/src/main.ts to use the new HTTP exception filter and set up Swagger documentation. These changes were made to improve error handling and provide API documentation. No breaking changes expected.
    
    ref #backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,20 +1,29 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './infrastructure/filters/http-exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // const globalPrefix = 'api';
-  // app.setGlobalPrefix(globalPrefix);
+  
+  app.useGlobalFilters(new HttpExceptionFilter());
+  const config = new DocumentBuilder()
+    .setTitle('Pokedex API')
+    .setDescription('API para gerenciamento de Pokémons com integração à API POKEDEX')
+    .setVersion('1.0')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+  
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/`
+  );
+  Logger.log(
+    `📝 Swagger documentation is available at: http://localhost:${port}/api/docs`
   );
 }
 
